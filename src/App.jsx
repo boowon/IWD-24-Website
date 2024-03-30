95a4b54f5f91de823ec57db1340ad2b1ebd7e309
Dyna-ALISMAIL <ld_alismail@esi.dz>
2024-03-30 01:01:13 +0100

    Register page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Home from './Pages/Home';
 import Navbar from './Pages/Navbar';
-import WhatISIWD from './Pages/WhatIsIWD';
+import WhatISIWD from './Pages/WhatISIWD';
 import FAQ from './Pages/FAQ';
 import Quiz from './Pages/QUIZ';
 import Register from './Pages/REGISTER';
