90714b595747746fefb0ffcaf922472ecc4ff4c1
boowon <94376439+boowon@users.noreply.github.com>
2024-04-02 20:40:15 +0100

    Update Navbar.jsx

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -13,7 +13,7 @@ function Navbar({activeSection}) {
   };
 
   return (
-    <nav className="bg-white sticky w-[100%] top-0 z-50  font-montserrat text-2xl font-medium">
+    <nav className="sticky w-[100%] top-0 z-50  font-montserrat text-2xl font-medium">
       <div className=" pt-5 px-4 lg:py-10 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
         <div className="hidden lg:block flex-shrink-0">
